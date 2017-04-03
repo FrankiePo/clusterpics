f0a5e41f7f22e0aa826910f47849bdca727e7be3
Sergey Koshechkin <serge.koshechkin@gmail.com>
2017-04-03 18:17:34 +0300

    add components and html layouts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import 'hammerjs';
+import { MaterialModule } from "@angular/material";
 
 import { AppComponent } from './app.component';
+import { MapComponent } from './map/map.component';
+import { TableComponent } from './table/table.component';
+import { ItemComponent } from './item/item.component';
+import { FormComponent } from './form/form.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    TableComponent,
+    ItemComponent,
+    FormComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MaterialModule,
     BrowserModule,
     FormsModule,
     HttpModule
